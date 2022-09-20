f94695696766584298a3c9878a6682bb80dbadd9
ITSC <ITSC@DESKTOP-ONE4R81>
2022-09-20 21:51:22 +0900

    change 7

--- src/day01/basic/MainClass07.java
+++ src/day01/basic/MainClass07.java
@@ -4,8 +4,8 @@ package day01.basic;
  * 1.\n : 줄바꿈(개행)
  * 2.\t : 탭기능
  * 3.\\ : \를 문자화
- * \' : '를 문자화
- * \" : "를 문자화 
+ * 4.\' : '를 문자화
+ * 5.\" : "를 문자화 
  * 
  * sdfds
  * 
@@ -15,10 +15,10 @@ package day01.basic;
 
 public class MainClass07 {
 	public static void main(String[] args) {
-		string str1 = "\"JavaStudy\"";
+		String str1 = "\"JavaStudy\"";
 		System.out.println(str1);
 	
-		String str2="안녕하세여. \n \'김태호\'입니다. \n \t 반갑습니다"
+		String str2="안녕하세여. \n \'김태호\'입니다. \n \t 반갑습니다";
 		System.out.println(str2);
 }
 	
